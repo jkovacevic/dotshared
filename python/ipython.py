2b79965262e62ec8a9c84fe734751dbfa52d1ed6
jkovacevic <janko.kovacevic@smaato.com>
2020-05-13 00:15:34 +0200

    automated commit message

--- python/ipython.py
+++ python/ipython.py
@@ -1,10 +1,10 @@
-try:
-	import pandas as pd
-	pd.set_option('display.max_rows', 0)
-	pd.set_option('display.max_columns', 0)
-except:
-	pass
-    
+def setup_pandas():
+    import pandas as pd
+    pd.set_option('display.max_rows', 0)
+    pd.set_option('display.max_columns', 0)
+
+def varz():
+    print("- Variables:\n{}\n".format("\n".join([x for x in  vars() if "_" not in x])))
 
 c.InteractiveShell.color_info = True
 c.InteractiveShell.colors = 'Linux'
