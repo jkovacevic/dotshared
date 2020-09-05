95ad75688de746c98f54759e89d5e261deccf69a
jkovacevic <jk.simunic@gmail.com>
2020-09-06 01:57:17 +0200

    automated commit message

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# shared
+# Packages
+scrot
+tesseract
+xsel
+translate-shell
+
+# Additional notes
+In order for OCR-{EN,DE} to work it is required to move the models to folders:
+`sudo mv deu.traineddata /usr/share/tessdata`
+`sudo mv eng.traineddata /usr/share/tessdata`
--- bash/ocr.sh
+++ bash/ocr.sh
@@ -1,7 +1,7 @@
 IMAGE_NAME=/tmp/_.png
 FILE_NAME=/tmp/_.txt
 FILE_TMP=/tmp/__.txt
-x=$IMAGE_NAME && touch $x && import $x;
+scrot -s $IMAGE_NAME
 tesseract -l eng --dpi 200 $IMAGE_NAME $(echo $FILE_NAME | awk '{split($0, a, "."); print a[1]}')
 sed '/^[[:space:]]*$/d' $FILE_NAME > $FILE_TMP
 cat $FILE_TMP | xclip -selection clipboard
